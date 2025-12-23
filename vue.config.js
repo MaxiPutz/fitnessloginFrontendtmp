a56b67e2d2a664af37c77aed72f084e95089e7e8
Maximilian Putz <max@Mac.home>
2025-12-23 10:14:50 +0100

    change build setup

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-   publicPath: process.env.VUE_APP_PUBLIC_PATH || "/",
-  devServer: {
-    proxy: "http://localhost:8080/fitness/api/login/"
-  }
+  publicPath: "/fitness/login/"
 })
 
 
